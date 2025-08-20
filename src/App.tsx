cf4ee154b3816b3852373d37951244115a6daba0
LeeYa Master <1162091199@qq.com>
2025-08-20 18:25:06 +0800

    chore: integrate react-hook-form zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button"
+import Test from "./Test/test"
  
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        {/* <Button>Click me</Button> */}
+        <Test />
+      </div>
   )
 }
  
---
+++ src/Test/test.tsx
@@ -0,0 +1,90 @@
+import { Button } from "@/components/ui/button";
+import {
+  FormField,
+  FormItem,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormLabel,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import DatePickerCombo from "@/components/myComponents/datePickerCombo";
+
+import { z } from "zod";
+import dayjs from "dayjs";
+
+enum GenderEnum {
+  female = "female",
+  male = "male",
+  other = "other",
+}
+
+interface IFormInput {
+  firstName: string;
+  gender: GenderEnum;
+}
+function Test() {
+  const form = useForm<IFormInput>();
+  const onSubmit = (data: IFormInput) => {
+
+    const schema = z.object({
+      username: z.string().min(1, "Username不能为空"),
+      date: z.date({ message: "日期不能为空" }),
+    });
+
+    const result = schema.safeParse(data);
+    if (!result.success) {
+      console.log("Form validation failed: " + JSON.stringify(result.error.format(), null, 2));
+      return;
+    }
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="shadcn"
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>
+
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => {
+              if (!field.value) {
+                field.onChange(dayjs().toDate());
+              }
+              return (
+                <DatePickerCombo field={field} />
+              );
+            }}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+}
+
+export default Test;
